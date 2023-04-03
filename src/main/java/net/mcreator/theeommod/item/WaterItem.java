a31be115834c43100c083a80bd62c3b5c1ad5be6
CreateSource <abnormalhare822@gmail.com>
2023-04-02 20:58:15 -0400

    commit

---
+++ src/main/java/net/mcreator/theeommod/item/WaterItem.java
@@ -0,0 +1,30 @@
+
+package net.mcreator.theeommod.item;
+
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+
+import net.mcreator.theeommod.init.TheEomModModTabs;
+
+public class WaterItem extends Item {
+	public WaterItem() {
+		super(new Item.Properties().tab(TheEomModModTabs.TAB_EOM_MOD).stacksTo(64).rarity(Rarity.COMMON));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.EAT;
+	}
+
+	@Override
+	public boolean hasCraftingRemainingItem() {
+		return true;
+	}
+
+	@Override
+	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
+		return new ItemStack(this);
+	}
+}
